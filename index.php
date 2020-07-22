19c4143fb39c9e67c3cca6e3e9dbf9068220681b
root <root@main.hosta.by>
2020-07-22 17:58:11 +0300

    22-07-03 ver

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1> Hello from K8s</h1><br>";
 echo "Server IP is: $ip_server", "<br><p>";
-echo "Made by Zarax and git";
-echo "Ver: 22.07";
+echo "Made by Zarax and git", "<br><p>";
+echo "Ver: 22-07-03";
 
 ?>
